0abcecd309ccbc964e916118a10c7b72a4b6e841
sheawilson1 <sheawilson207850@gmail.com>
2026-05-01 10:11:19 +0100

    Support Vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/smart-home-2065-obi-demo/',
+  base: process.env.VERCEL ? '/' : '/smart-home-2065-obi-demo/',
   plugins: [react()],
   server: {
     host: '127.0.0.1',
